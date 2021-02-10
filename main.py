d7179c6f94155b5da6c7d3c017ed8af3c066bafd
Cade Lanktree <lanktree.cade@gmail.com>
2021-02-09 22:44:20 -0500

    imported dependencies

---
+++ main.py
@@ -0,0 +1,14 @@
+#inspired by: https://www.youtube.com/watch?v=iGWbqhdjf2s
+
+#import modules
+import tensorflow as tf
+import numpy as np
+import matplotlib.pyplot as plt
+from tensorflow import keras
+from keras.models import Sequential
+from keras.layers import Dense, Flatten, Conv2D, MaxPooling2D, Dropout
+from tensorflow.keras import layers
+from keras.utils import to_categorical
+plt.style.use('fivethirtyeight')
+
+
