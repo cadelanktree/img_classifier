108c04b4ddc5c96ad0dd709ed2f4dd3babfa0e6c
Cade Lanktree <lanktree.cade@gmail.com>
2021-02-10 16:14:31 -0500

    basic classificaiton achieved

--- main.py
+++ main.py
@@ -11,4 +11,24 @@ from tensorflow.keras import layers
 from keras.utils import to_categorical
 plt.style.use('fivethirtyeight')
 
+#load data
+from keras.datasets import cifar10
+(x_train, y_train), (x_test, y_test) = cifar10.load_data()
+
+
+
+#look at data types of vars
+print(type(x_train))
+print(type(y_train))
+print(type(x_test))
+print(type(y_test))
+
+#Get shape of the arrays
+print(x_train.shape)
+print(y_train.shape)
+print(x_test.shape)
+print(y_test.shape)
+
+classification = ['airplane', 'automobile', 'bird', 'cat', 'deer', 'frog', 'horse', 'ship', 'truck']
+print('The image class is:', classification[y_train[0][0]])
 
